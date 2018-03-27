e0bcffb914ebde463822f9f13669a981c00bf7bc
calobo <calobo@wpi.edu>
2018-03-27 13:00:44 -0400

    [Commit Message]

--- app/src/main/java/com/example/water/waterwatcherz/MainActivity.java
+++ app/src/main/java/com/example/water/waterwatcherz/MainActivity.java
@@ -9,7 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         // :D
+
+        // PEW PEW NOT BORING COMMENT
     }
 }
