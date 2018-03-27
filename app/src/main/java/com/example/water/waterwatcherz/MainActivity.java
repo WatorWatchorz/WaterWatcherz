7aac522021b5d038698662222455ea19f6b8f233
nathankrish <nathan.krish.edu@gmail.com>
2018-03-27 12:05:24 -0400

    bippity boo

--- app/src/main/java/com/example/water/waterwatcherz/MainActivity.java
+++ app/src/main/java/com/example/water/waterwatcherz/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-//shoe
+//shoee
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
