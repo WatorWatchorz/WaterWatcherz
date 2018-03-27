93e74631923bd1ecac344cfb56b58097a93b8b20
gnandyalam <gnandyalam@wpi.edu>
2018-03-27 12:15:03 -0400

    HI CHRIS

--- app/src/main/java/com/example/water/waterwatcherz/MainActivity.java
+++ app/src/main/java/com/example/water/waterwatcherz/MainActivity.java
@@ -10,6 +10,10 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //
+        System.out.println("UNDA DA C");
+        System.out.println("UNDA DA C");
+        System.out.println("UNDA DA C");
+        System.out.println("UNDA DA C");
+
     }
 }
