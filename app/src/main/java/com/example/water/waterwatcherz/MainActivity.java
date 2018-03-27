bba4f3fabb5bfd968de00baccaca3ee8a9840184
calobo <calobo@wpi.edu>
2018-03-27 11:50:41 -0400

    I CAN SHOW YOU THE WORLD

--- app/src/main/java/com/example/water/waterwatcherz/MainActivity.java
+++ app/src/main/java/com/example/water/waterwatcherz/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("LOLOLOLOLOLOL");
+        System.out.println("Chris is testing GitHub");
     }
 }
