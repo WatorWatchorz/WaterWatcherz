0d7c6bdc06429a1b2e5dfe767fc3b240e3ed3049
eeshav <eeshaverma001@gmail.com>
2018-03-28 08:58:47 -0400

    :D

---
+++ app/src/main/java/com/example/water/waterwatcherz/WaterTask.java
@@ -0,0 +1,16 @@
+package com.example.water.waterwatcherz;
+
+/**
+ * Stores the day and time of a water task, the name of the task (selectable category),
+ * and the duration of the task
+ * Created by eesha on 3/28/2018.
+ */
+
+public class WaterTask
+{
+    //Data
+
+    //Constructor
+
+    //Methods
+}
