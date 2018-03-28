241be8640e837981cc5fa160621e11239b18daa2
eeshav <eeshaverma001@gmail.com>
2018-03-28 09:05:31 -0400

    :D

---
+++ app/src/main/java/com/example/water/waterwatcherz/User.java
@@ -0,0 +1,15 @@
+package com.example.water.waterwatcherz;
+
+/**
+ * Contains WaterTask objects and data on a user's household size, zip code, and weekly water goal
+ * Created by eesha on 3/28/2018.
+ */
+
+public class User
+{
+    //Data
+
+    //Constructor
+
+    //Methods
+}
