0753eed931ef17a6f98e7c5ddbaaf3972f97b28b
eeshav <eeshaverma001@gmail.com>
2018-03-28 09:07:33 -0400

    :D

---
+++ app/src/main/java/com/example/water/waterwatcherz/Group.java
@@ -0,0 +1,15 @@
+package com.example.water.waterwatcherz;
+
+/**
+ * Contains User objects, specifies the owner of a group, and has a unique group code
+ * Created by eesha on 3/28/2018.
+ */
+
+public class Group
+{
+    //Data
+
+    //Constructor
+
+    //Methods
+}
