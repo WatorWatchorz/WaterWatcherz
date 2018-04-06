56b0c9e7902346fef58fd1c703ce1def6dc517a8
gnandyalam <gnandyalam@wpi.edu>
2018-04-06 12:56:59 -0400

    Merge branch 'master' of https://github.com/WatorWatchorz/WaterWatcherz
    # Conflicts:
    #	app/src/main/java/com/example/water/waterwatcherz/MainActivity.java

--- app/src/main/java/com/example/water/waterwatcherz/UserTest.java
+++ app/src/main/java/com/example/water/waterwatcherz/UserTest.java
@@ -1,5 +1,9 @@
 package com.example.water.waterwatcherz;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by gnandyalam on 4/2/18.
  */
@@ -7,10 +11,15 @@ package com.example.water.waterwatcherz;
 public class UserTest {
 
     public static void main(String[] args) {
-        User user = new User("Showering", 4, "01605", 15);
+        ArrayList<WaterTask> testTask = new ArrayList<WaterTask>();
+        WaterTask activity = new WaterTask("Shower", 1, 3, 2018, 1, 2, 8, 7);
+        testTask.add(activity);
+        User user = new User(testTask, 4, "01605", 15, "Chris");
         System.out.println(user.getTask());
         System.out.println(user.getHouseSize());
         System.out.println(user.getZip());
         System.out.println(user.getWeeklyGoal());
+        System.out.println(user.getName());
+
     }
 }
