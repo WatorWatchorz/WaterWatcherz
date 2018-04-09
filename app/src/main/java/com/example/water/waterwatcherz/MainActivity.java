bd25de440e5e7669b8dd6c815c0372cff9154c10
calobo <calobo@wpi.edu>
2018-04-09 08:33:05 -0400

    [Commit Message]

--- app/src/main/java/com/example/water/waterwatcherz/MainActivity.java
+++ app/src/main/java/com/example/water/waterwatcherz/MainActivity.java
@@ -10,4 +10,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public static void main(String [] args)
+    {
+        User bob = new User();
+        Group g = new Group();
+
+        System.out.println("GROUP CODE: " + g.getGroupCode());
+        System.out.println("OWNER: " + g.getOwner());
+        System.out.print("USERS: " + g.getUsers());
+        g.addUser(bob);
+        g.setOwner(bob);
+        g.setGroupCode(234);
+        System.out.println("");
+        System.out.println("GROUP CODE: " + g.getGroupCode());
+        System.out.println("OWNER: " + g.getOwner());
+        System.out.print("USERS: " + g.getUsers());
+    }
 }
