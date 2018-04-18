386453e5b25796d871932f276b06d8c0a53d9676
gnandyalam <gnandyalam@wpi.edu>
2018-04-18 13:16:30 -0400

    Merge branch 'master' of https://github.com/WatorWatchorz/WaterWatcherz
    # Conflicts:
    #	app/src/main/java/com/example/water/waterwatcherz/MainActivity.java

--- app/src/main/java/com/example/water/waterwatcherz/User.java
+++ app/src/main/java/com/example/water/waterwatcherz/User.java
@@ -202,6 +202,19 @@ public class User {
         brushTeethTime = brushTime;
     }
 
+    public void leavesWaterOn() {
+        String ans = "Yes";
+
+        if (ans == "Yes") {
+           getBrushTeethNum();
+           getBrushTeethTime();
+        }
+        else {
+            
+        }
+
+    }
+
     /**
      * Accesses the number of times a user brushes their teeth
      *
