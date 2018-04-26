5494d31232cc99f76385bac4ea06271c6384f341
eeshav <eeshaverma001@gmail.com>
2018-04-26 09:27:18 -0400

    Merge branch 'master' of https://github.com/WatorWatchorz/WaterWatcherz
    # Conflicts:
    #	app/src/main/java/com/example/water/waterwatcherz/UserTest.java

--- app/src/main/java/com/example/water/waterwatcherz/AddTaskActivity.java
+++ app/src/main/java/com/example/water/waterwatcherz/AddTaskActivity.java
@@ -2,6 +2,8 @@ package com.example.water.waterwatcherz;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -9,7 +11,7 @@ import android.widget.Spinner;
  * Created by krish on 4/12/2018.
  */
 
-public class AddTaskActivity  extends AppCompatActivity {
+public class AddTaskActivity  extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +20,25 @@ public class AddTaskActivity  extends AppCompatActivity {
 
 
         Spinner taskSpinner = (Spinner) findViewById(R.id.taskDropDown);
-        String[] tasks = new String[]{"Brushing Teeth", "Shower", "Bath", "Watering Lawn", "Filling Pool",
-                "Washing Dishes", "Laundry"};
+//        String[] tasks = new String[]{"Brushing Teeth", "Shower", "Bath", "Watering Lawn", "Filling Pool",
+//                "Washing Dishes", "Laundry"};
         ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
                 R.array.tasks_array, android.R.layout.simple_spinner_item);
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         taskSpinner.setAdapter(adapter1);
 
+        taskSpinner.setOnItemSelectedListener(this);
+
     }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
 }
