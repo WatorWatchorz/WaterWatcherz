636e6ad38e2c6673bfb34c2f70a7c8a842e455b8
eeshav <eeshaverma001@gmail.com>
2018-05-01 08:06:53 -0400

    :D

--- app/src/main/java/com/example/water/waterwatcherz/HouseInfoActivity.java
+++ app/src/main/java/com/example/water/waterwatcherz/HouseInfoActivity.java
@@ -2,6 +2,9 @@ package com.example.water.waterwatcherz;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -11,18 +14,36 @@ import java.io.File;
  * Created by krish on 4/10/2018.
  */
 
-public class HouseInfoActivity extends AppCompatActivity {
+public class HouseInfoActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener
+{
     protected void onCreate(Bundle savedInstanceState) {
-        Bundle bundle = getIntent().getExtras();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.house_info);
-    }
-    public void storeInformation() {
+        Bundle bundle = getIntent().getExtras();
+
+
         Spinner townSpinner = findViewById(R.id.townDropDown);
-        EditText numPplEdit = findViewById(R.id.NumPplEDT);
-        String numPplStr = numPplEdit.getText().toString();
-        if (!(numPplStr.isEmpty())) {
-            int numPpl = Integer.parseInt(numPplStr);
-        }
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.towns_array, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        townSpinner.setAdapter(adapter2);
+        townSpinner.setOnItemSelectedListener(this);
     }
+
+//  public void storeInformation() {
+// EditText numPplEdit = findViewById(R.id.NumPplEDT);
+//        String numPplStr = numPplEdit.getText().toString();
+//        if (!(numPplStr.isEmpty())) {
+//            int numPpl = Integer.parseInt(numPplStr);
+//        }
+//    }
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
 }
