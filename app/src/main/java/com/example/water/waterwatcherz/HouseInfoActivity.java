c5c8d2f6f5db01eb3cbb78a9c5393db4755075f9
eeshav <eeshaverma001@gmail.com>
2018-05-07 09:16:01 -0400

    Merge branch 'master' of https://github.com/WatorWatchorz/WaterWatcherz
    # Conflicts:
    #	app/src/main/java/com/example/water/waterwatcherz/HouseInfoActivity.java
    #	app/src/main/java/com/example/water/waterwatcherz/MainActivity.java
    #	app/src/main/java/com/example/water/waterwatcherz/UtilityInformationActivity.java

--- app/src/main/java/com/example/water/waterwatcherz/HouseInfoActivity.java
+++ app/src/main/java/com/example/water/waterwatcherz/HouseInfoActivity.java
@@ -16,65 +16,65 @@ import java.io.File;
  * Created by krish on 4/10/2018.
  */
 
-public class HouseInfoActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener
+public class HouseInfoActivity // extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
-
-    private Button taskActivityButton_houseinfo;
-    private Button utilitybutton_houseinfo;
-
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.house_info);
-        Bundle bundle = getIntent().getExtras();
-
-        Spinner townSpinner = findViewById(R.id.townDropDown);
-        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
-                R.array.towns_array, android.R.layout.simple_spinner_item);
-        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        townSpinner.setAdapter(adapter2);
-        townSpinner.setOnItemSelectedListener(this);
+//                UNUSED ACTIVITY
 
 
-        taskActivityButton_houseinfo = (Button) findViewById(R.id.calendar_houseinfo);
-        utilitybutton_houseinfo = (Button) findViewById(R.id.utilitybutton_houseinfo);
-
-        taskActivityButton_houseinfo.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                openTaskActivity();
-            }
-        });
-        utilitybutton_houseinfo.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                openUtiltyInformation();
-            }
-        });
-    }
-
-//  public void storeInformation() {
-// EditText numPplEdit = findViewById(R.id.NumPplEDT);
-//        String numPplStr = numPplEdit.getText().toString();
-//        if (!(numPplStr.isEmpty())) {
-//            int numPpl = Integer.parseInt(numPplStr);
-//        }
+//    private Button taskActivityButton_houseinfo;
+//    private Button utilitybutton_houseinfo;
+//
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.settings);
+//        Bundle bundle = getIntent().getExtras();
+//
+//        Spinner townSpinner = findViewById(R.id.townDropDown);
+//        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+//                R.array.towns_array, android.R.layout.simple_spinner_item);
+//        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        townSpinner.setAdapter(adapter2);
+//        townSpinner.setOnItemSelectedListener(this);
+//
+//        taskActivityButton_houseinfo = (Button) findViewById(R.id.calendar_houseinfo);
+//        utilitybutton_houseinfo = (Button) findViewById(R.id.utilitybutton_houseinfo);
+//
+//        taskActivityButton_houseinfo.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {openTaskActivity();
+//            }
+//        });
+//        utilitybutton_houseinfo.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                openUtiltyInformation();
+//            }
+//        });
 //    }
-
-
-    public void openTaskActivity() {
-        Intent intent = new Intent(this,AddTaskActivity.class);
-        startActivity(intent);
-    }
-    public void openUtiltyInformation() {
-        Intent intent = new Intent(this,UtilityInformationActivity.class);
-        startActivity(intent);
-    }
-
-    public void onItemSelected(AdapterView<?> parent, View view,
-                               int pos, long id) {
-        // An item was selected. You can retrieve the selected item using
-        // parent.getItemAtPosition(pos)
-    }
-    public void onNothingSelected(AdapterView<?> parent) {
-        // Another interface callback
-   }
+//
+////  public void storeInformation() {
+//// EditText numPplEdit = findViewById(R.id.NumPplEDT);
+////        String numPplStr = numPplEdit.getText().toString();
+////        if (!(numPplStr.isEmpty())) {
+////            int numPpl = Integer.parseInt(numPplStr);
+////        }
+////    }
+//
+//
+//    public void openTaskActivity() {
+//        Intent intent = new Intent(this,AddTaskActivity.class);
+//        startActivity(intent);
+//    }
+//    public void openUtiltyInformation() {
+//        Intent intent = new Intent(this,UtilityInformationActivity.class);
+//        startActivity(intent);
+//    }
+//
+//    public void onItemSelected(AdapterView<?> parent, View view,
+//                               int pos, long id) {
+//        // An item was selected. You can retrieve the selected item using
+//        // parent.getItemAtPosition(pos)
+//    }
+//    public void onNothingSelected(AdapterView<?> parent) {
+//        // Another interface callback
+//   }
 
 }
--- app/src/main/java/com/example/water/waterwatcherz/UtilityInformationActivity.java
+++ app/src/main/java/com/example/water/waterwatcherz/UtilityInformationActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -15,28 +16,41 @@ import android.widget.EditText;
 
 public class UtilityInformationActivity  extends AppCompatActivity {
 
-    private Button profilebutton_addtask;
-    private Button utilitybutton_addtask;
+    private Button profileButton_utilityinfo;
+    private Button taskActivityButton_utilityinfo;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.utility_bill_information);
         Bundle bundle = getIntent().getExtras();
+
+        profileButton_utilityinfo = (Button) findViewById(R.id.profile);
+        taskActivityButton_utilityinfo = (Button) findViewById(R.id.calendar);
+
+        profileButton_utilityinfo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                openProfile();
+            }
+        });
+        taskActivityButton_utilityinfo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {openTaskActivity();
+            }
+        });
     }
 
-    public void storeWaterUsed(){
-        EditText waterUsed = (EditText) findViewById(R.id.waterUsed);
-       Double waterUsedDbl = 0.0;
-        if (!waterUsed.toString().isEmpty()) {
-            waterUsedDbl = Double.parseDouble(waterUsed.toString());
-        }
-        /*
-        Intent intent = new Intent (this, SQLHandler.class);
-
-        intent.putExtra("Water Used",waterUsedDbl);
-        startActivity(intent);
-        */
-        }
+//    public void storeWaterUsed(){
+//        EditText waterUsed = (EditText) findViewById(R.id.waterUsed);
+//       Double waterUsedDbl = 0.0;
+//        if (!waterUsed.toString().isEmpty()) {
+//            waterUsedDbl = Double.parseDouble(waterUsed.toString());
+//        }
+//        /*
+//        Intent intent = new Intent (this, SQLHandler.class);
+//
+//        intent.putExtra("Water Used",waterUsedDbl);
+//        startActivity(intent);
+//        */
+//        }
 
     public void openProfile() {
         Intent intent = new Intent(this,ProfileActivity.class);
@@ -47,7 +61,5 @@ public class UtilityInformationActivity  extends AppCompatActivity {
         Intent intent = new Intent(this,AddTaskActivity.class);
         startActivity(intent);
     }
-
-
 
 }
